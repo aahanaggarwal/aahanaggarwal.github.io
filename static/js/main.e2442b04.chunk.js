f9206c8c34169a3a6ade4448e599b9bce5687c5b
Aahan Aggarwal <aahan.aggarwal99@gmail.com>
2021-07-10 23:23:36 -0400

    build now should actually work

---
+++ static/js/main.e2442b04.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var r=n(1),c=n.n(r),s=n(3),a=n.n(s),i=(n(8),n(0));var o=function(){return Object(i.jsx)("header",{children:"Aahan Aggarwal"})};a.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.e2442b04.chunk.js.map
