d1f21e89002bb5563e58a23ce32abb5f0b673be8
Aahan Aggarwal <aahan.aggarwal99@gmail.com>
2021-10-23 22:54:28 -0400

    simple

--- website/src/index.js
+++ website/src/index.js
@@ -1,32 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Header from "./Header";
-import WorkExp from "./WorkExp";
-import LinkBar from "./LinkBar";
-import Pictures from "./Pictures";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
-    <Container>
-      <Row>
-        <LinkBar />
-      </Row>
-      <Row>
-        <Col fluid>
-          <Pictures />
-        </Col>
-        <Col fluid>
-          <WorkExp />
-        </Col>
-      </Row>
-    </Container>
+    <p>
+      Hi, my name is Aahan Aggarwal.<br/>
+      I'm from Mumbai, and I like computers.<br/>
+      You can contact me at <a href="mailto:me@aahan.dev">me@aahan.dev</a>.
+    </p>
+
+    <p>
+      Present:<br/>
+      Completing my Master's in CS at <a href="https://www.purdue.edu">Purdue University</a>.
+    </p>
+    <p>
+      Past:<br/>
+      Interned at <a href="https://www.facebook.com">Facebook</a> - GraphQL Build Infra and WhatsApp Core Infrastructure teams.<br/>
+      Completed my Bachelor's in CS at Purdue University.
+    </p>
+
+    <p>
+      Some Links:<br/>
+      <a href="https://www.linkedin.com/in/aahanaggarwal99/">LinkedIn</a><br/>
+      <a href="https://twitter.com/aahanaggarwal">Twitter</a><br/>
+      <a href="https://github.com/aahanaggarwal/">GitHub</a><br/>
+      <a href="https://purdue0-my.sharepoint.com/:b:/g/personal/aggarw57_purdue_edu/EWKoU2JbI9tPuukrEwmrxrwBwKZGagOMTgSlOcYRBgcGiw">Resume</a>
+    </p>
   </React.StrictMode>,
   document.getElementById("root")
 );
